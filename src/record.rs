7b3420a698715742c382c08144a1c6ccd63dfa8e
Niclas <niclas.lind93@gmail.com>
2021-10-18 18:21:38 +0200

    added some standard tests

--- src/record.rs
+++ src/record.rs
@@ -1,5 +1,3 @@
-use std::io::Write;
-
 /// A record held LIN-frame data.
 ///
 /// `id` is equal to a LIN-frame ID
@@ -9,7 +7,7 @@ use std::io::Write;
 /// `master` tells if the frame is a master frame(1) or an arbitration frame(0)
 ///
 /// `cache_valid` tells if the cached_data is valid data for this record
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Record {
     id: u8,
     size: u8,
@@ -19,13 +17,14 @@ pub struct Record {
 }
 
 impl Record {
+    /// Create a new object of type [`Record`]
     pub fn new(id: u8, size: u8, master: u8) -> Self {
         Self {
             id,
             size,
             master,
             cache_valid: false,
-            write_cache: Vec::with_capacity(8),
+            write_cache: vec![0x00u8; 8],
         }
     }
 
@@ -50,11 +49,70 @@ impl Record {
     }
 
     pub fn set_write_cache(&mut self, data: &[u8]) {
-        self.write_cache.clear();
-        self.write_cache.write_all(data).unwrap();
+        // Check that the frame isn't greater then 8
+        assert!(data.len() <= 8, "Data frame is greater then 8");
+        self.write_cache[..data.len()].copy_from_slice(data);
     }
 
     pub fn cache(&self) -> &[u8] {
         &self.write_cache[..self.size as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Record;
+
+    #[test]
+    fn get_record_id() {
+        let record = Record::new(0x43, 5, 1);
+        assert_eq!(0x43, record.id());
+    }
+
+    #[test]
+    fn get_record_size() {
+        let record = Record::new(0x00, 4, 0);
+        assert_eq!(4, record.size());
+    }
+
+    #[test]
+    fn is_master_record() {
+        let master_record = Record::new(0x32, 6, 1);
+        let slave_record = Record::new(0x12, 8, 0);
+
+        assert!(master_record.is_master());
+        assert!(!slave_record.is_master());
+    }
+
+
+    #[test]
+    fn is_cache_valid() {
+        let mut record = Record::new(0x11, 5, 0);
+        assert!(!record.cache_valid());
+        record.set_cache_valid(true);
+        assert!(record.cache_valid());
+    }
+
+    #[test]
+    fn test_set_write_cache() {
+        let mut record = Record::new(0x00, 4, 0);
+
+        let data = [0x00u8, 0x32, 0x13, 0x23];
+
+        record.set_write_cache(&data);
+
+        for (index, value) in record.cache().iter().enumerate() {
+            assert_eq!(data[index], *value);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_set_write_cache_with_to_big_frame() {
+        let mut record = Record::new(0x00, 4, 0);
+
+        let data = [0x00u8, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08];
+        // This should panic, because the maximum frame length on LIN is 8
+        record.set_write_cache(&data);
     }
 }
--- src/records.rs
+++ src/records.rs
@@ -6,16 +6,25 @@ pub struct Records {
 }
 
 impl Records {
+    /// Create a new object of [`Records`] which held a list with capacity of 30
     pub fn new() -> Self {
         Self {
             record_list: Vec::with_capacity(30),
         }
     }
 
+    /// Set record_lsit to records
     pub fn set_list(&mut self, records: Vec<Record>) {
         self.record_list = records;
     }
 
+    /// Returning a clone of record_list
+    fn get_records(&self) -> Vec<Record> {
+        self.record_list.clone()
+    }
+
+    /// Search over the collection for a record with `id`, if match return an imutable [`Record`]
+    /// otherwise return None
     pub fn find_by_id_im(&self, id: u8) -> Option<Record> {
         for record in &self.record_list {
             if record.id() == id {
@@ -25,6 +34,8 @@ impl Records {
         None
     }
 
+    /// Search over the collection for for a record with `id`, if match return a mutable reference of [`Record`]
+    /// otherwise return None
     pub fn find_by_id(&mut self, id: u8) -> Option<&mut Record> {
         for record in &mut self.record_list {
             if record.id() == id {
@@ -33,5 +44,57 @@ impl Records {
         }
 
         None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{record::Record, records::Records};
+
+    #[test]
+    fn test_set_list() {
+        // Create a Record object
+        let mut records = Records::new();
+
+        // Create a list with records
+        let list = vec![Record::new(0x00, 5, 0), Record::new(0x02, 2, 1)];
+
+        // Add it to the records object
+        records.set_list(list.clone());
+
+        // Assert that everything is in place
+        assert_eq!(list[0], records.get_records()[0]);
+        assert_eq!(list[1], records.get_records()[1]);
+    }
+
+    #[test]
+    fn test_find_by_id() {
+        // Creata a Record object
+        let mut records = Records::new();
+
+        // Create a list with records
+        let list = vec![Record::new(0x00, 5, 0), Record::new(0x02, 2, 1)];
+
+        records.set_list(list.clone());
+
+        assert_eq!(records.find_by_id(0x00), Some(&mut list[0].clone()));
+        assert_eq!(records.find_by_id(0x01), None);
+        assert_eq!(records.find_by_id(0x02), Some(&mut list[1].clone()));
+    }
+
+    #[test]
+    fn test_find_by_id_im() {
+        // Create a Record object
+        let mut records = Records::new();
+
+        // Create a list with records
+        let list = vec![Record::new(0x00, 5, 0), Record::new(0x02, 2, 1)];
+
+        // Add it to the records object
+        records.set_list(list.clone());
+
+        assert_eq!(records.find_by_id_im(0x00), Some(list[0].clone()));
+        assert_eq!(records.find_by_id_im(0x01), None);
+        assert_eq!(records.find_by_id_im(0x02), Some(list[1].clone()));
     }
 }
