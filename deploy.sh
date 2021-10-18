0c168b731267402f7693ec62ea30788f0c97d139
Niclas <niclas.lind93@gmail.com>
2021-10-18 11:24:50 +0200

    changed the deploy to build a release binary

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ set -o xtrace
 readonly PROJECT_NAME=signalbroker-lin-transceiver-rp
 readonly TARGET_HOST=pi@$1
 readonly TARGET_PATH=/home/pi/$PROJECT_NAME
-readonly SOURCE_PATH=./target/armv7-unknown-linux-gnueabihf/debug/$PROJECT_NAME
+readonly SOURCE_PATH=./target/armv7-unknown-linux-gnueabihf/release/$PROJECT_NAME
 
-cargo build 
+cargo build --release
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
 ssh -t ${TARGET_HOST} ${TARGET_PATH}
